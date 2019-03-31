0d5ae18be96ee1fa588e6ced5677e25823c107cd
Evan Feenstra <evan@evanfeenstra.com>
2019-03-31 12:15:16 -0700

    attachHttpServer for mosca

--- broker.js
+++ broker.js
@@ -99,7 +99,7 @@ async function init() {
         interfaces: [
             { type: "mqtt", port: parseInt(MQTT_PORT) || 1883 },
             // { type: "mqtts", port: 8883, credentials: { keyPath: SECURE_KEY, certPath: SECURE_CERT } },
-            { type: "http", port: parseInt(WS_PORT) || 80, bundle: true },
+            // { type: "http", port: parseInt(WS_PORT) || 80, bundle: true },
             // { type: "https", port: 3001, bundle: true, credentials: { keyPath: SECURE_KEY, certPath: SECURE_CERT } }
         ],
         stats: false,
